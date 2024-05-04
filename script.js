6299d1d6db3dc3b188d06548fa40420be8dbafb1
camicollado <cacollado@itba.edu.ar>
2024-05-03 21:13:31 -0300

    javascript parte1

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-console.log("hola")
-//let nombre=prompt("nombre")
-//alert("hola "+nombre)
+const resultadosReducidos = [
+    {"app": "Instagram", "predModelo": 4, "ratingReal": 4},
+    {"app": "Whatsapp", "predModelo": 4, "ratingReal": 4},
+    {"app": "TikTok", "predModelo": 5, "ratingReal": 4},
+    {"app": "Cuando subo", "predModelo": 1, "ratingReal": 2},
+    {"app": "Dia: La app de ofertas", "predModelo": 2, "ratingReal": 3},
+    {"app": "Mercado Pago", "predModelo": 3, "ratingReal": 4},
+    {"app": "Roblox", "predModelo": 4, "ratingReal": 4},
+    {"app": "Pou", "predModelo": 3, "ratingReal": 4},
+    {"app": "Subway Surfers", "predModelo": 4, "ratingReal": 4},
+    {"app": "Flow", "predModelo": 5, "ratingReal": 3},
+    {"app": "iLovePDF", "predModelo": 4, "ratingReal": 4},
+    {"app": "Uber Eats", "predModelo": 4, "ratingReal": 4},
+    {"app": "Calm (meditacion)", "predModelo": 3, "ratingReal": 4}
+  ]
+
+  console.table(resultadosReducidos)
+
+  
+  
